817f1398ec307021d384df90b00c03148e706d07
1r34k2 <irekkamalutdinov@yandex.ru>
2022-10-17 21:22:43 +0300

    laba

---
+++ lab2.ch_m/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace lab2.ch_m
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lvDir = new System.Windows.Forms.ListView();
+            this.rtbConsole = new System.Windows.Forms.RichTextBox();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.btnDirBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvDir
+            // 
+            this.lvDir.HideSelection = false;
+            this.lvDir.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.lvDir.Location = new System.Drawing.Point(13, 13);
+            this.lvDir.Name = "lvDir";
+            this.lvDir.Size = new System.Drawing.Size(775, 284);
+            this.lvDir.TabIndex = 0;
+            this.lvDir.UseCompatibleStateImageBehavior = false;
+            this.lvDir.View = System.Windows.Forms.View.List;
+            // 
+            // rtbConsole
+            // 
+            this.rtbConsole.Location = new System.Drawing.Point(13, 304);
+            this.rtbConsole.Name = "rtbConsole";
+            this.rtbConsole.ReadOnly = true;
+            this.rtbConsole.Size = new System.Drawing.Size(775, 107);
+            this.rtbConsole.TabIndex = 1;
+            this.rtbConsole.Text = "";
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(13, 418);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(694, 20);
+            this.tbInput.TabIndex = 2;
+            // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(713, 418);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(75, 23);
+            this.btnInput.TabIndex = 3;
+            this.btnInput.Text = "Ввод";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // btnDirBack
+            // 
+            this.btnDirBack.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDirBack.BackgroundImage")));
+            this.btnDirBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDirBack.Location = new System.Drawing.Point(765, 304);
+            this.btnDirBack.Name = "btnDirBack";
+            this.btnDirBack.Size = new System.Drawing.Size(23, 23);
+            this.btnDirBack.TabIndex = 4;
+            this.btnDirBack.UseVisualStyleBackColor = true;
+            this.btnDirBack.Click += new System.EventHandler(this.btnDirBack_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDirBack);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.rtbConsole);
+            this.Controls.Add(this.lvDir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView lvDir;
+        private System.Windows.Forms.RichTextBox rtbConsole;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.Button btnDirBack;
+    }
+}
+
---
+++ lab2.ch_m/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab2.ch_m
+{
+    public partial class Form1 : Form
+    {
+        private string dirName = "Q:\\2";
+        private string message = "";
+        string[] files;
+        string[] folders;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(Directory.Exists(dirName))
+            {
+                DirShow();
+            }
+        }
+
+        public void DirShow()
+        {
+            if (Directory.GetParent(dirName) == null)
+            {
+                btnDirBack.Enabled = false;
+            }
+            else
+            {
+                btnDirBack.Enabled = true;
+            }
+            lvDir.Clear();
+            files = Directory.GetFiles(dirName);
+            folders = Directory.GetDirectories(dirName);
+            foreach (string file in files)
+            {
+                string[] s = file.Split('\\');
+                lvDir.Items.Add(s[s.Length - 1]);
+            }
+            foreach (string folder in folders)
+            {
+                string[] s = folder.Split('\\');
+                lvDir.Items.Add(s[s.Length - 1]);
+            }
+            rtbConsole.Text += dirName + "\n";
+        }
+
+        private void DirChange(string dir)
+        {
+            try
+            {
+                if(!Directory.Exists(@dir))
+                {
+                    throw new DirException("Ошибка. Заданного каталога не существует.");
+                }
+                else
+                {
+                    dirName = @dir;
+                    message = "Каталог успешно изменён.\n";
+                }
+            }
+            catch
+            {
+                message = "Ошибка. Заданный каталог не существует.\n";
+            }
+            finally
+            {
+                rtbConsole.Text += message;
+            }
+        }
+
+        private void DirBack()
+        {
+            try
+            {
+                if (Directory.GetParent(dirName) == null)
+                {
+                    throw new DirException("Ошибка. Это родительский каталог");
+                }
+                else
+                {
+                    dirName = Directory.GetParent(dirName).ToString();
+                }
+            }
+            catch
+            {
+                message = "Ошибка. Это родительский каталог.\n";
+            }
+            finally
+            {
+                rtbConsole.Text += message;
+                DirShow();
+            }
+        }
+
+        private void Del(string target)
+        {
+            string[] filer;
+            try
+            {
+                foreach (string file in files)
+                {
+                    filer = file.Split('\\');
+                    if (filer[filer.Length - 1] == target) File.Delete(file);
+                }
+                foreach (string dir in folders)
+                {
+                    filer = dir.Split('\\');
+                    if (filer[filer.Length - 1] == target) Directory.Delete(dir);
+                }
+            }
+            catch
+            {
+                message = "Ошибка. Каталог не найден.\n";
+            }
+        }
+        private bool DelMany(string target)
+        {
+
+            try
+            {
+                if (target.IndexOf("*") == 0 && target.LastIndexOf("*") == target.Length - 1)
+                {
+                    foreach(string file in files)
+                    {
+                        if (file.Contains(target.Split('*')[1])) File.Delete(file);
+                    }
+                    foreach (string dir in folders)
+                    {
+                        if (dir.Contains(target.Split('*')[1])) Directory.Delete(dir);
+                    }
+                    return true;
+                }
+                else if(target.IndexOf("*") == 0)
+                {
+                    foreach (string file in files)
+                    {
+                        if (file.EndsWith(target.Split('*')[1])) File.Delete(file);
+                    }
+                    foreach (string dir in folders)
+                    {
+                        if (dir.EndsWith(target.Split('*')[1])) Directory.Delete(dir);
+                    }
+                    return true;
+                }
+                else
+                {
+                    foreach (string file in files)
+                    {
+                        if (file.StartsWith(target.Split('*')[0])) File.Delete(file);
+                    }
+                    foreach (string dir in folders)
+                    {
+                        if (dir.StartsWith(target.Split('*')[0])) Directory.Delete(dir);
+                    }
+                    return true;
+                }
+            }
+            catch
+            {
+                message = "Ошибка. Каталог не найден.\n";
+                return true;
+            }
+        }
+        
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            message = "";
+            rtbConsole.Text += tbInput.Text + "\n";
+            string[] command = tbInput.Text.Split(' ');
+            try
+            {
+                if (command[0] == "cd")
+                {
+                    try
+                    {
+                        if (command.Length != 2)
+                        {
+                            throw new ConsoleException("Синтаксическая ошибка.");
+                        }
+                        else if (command[1] == "..")
+                        {
+                            DirBack();
+                        }
+                        else
+                        {
+                            DirChange(command[1]);
+                        }
+                    }
+                    catch
+                    {
+                        message = "Синтаксическая ошибка. Пример: cd *path*\n";
+                        rtbConsole.Text += message;
+                    }
+                }
+
+                else if (command[0] == "rm")
+                {
+                    try
+                    {
+                        if (command.Length != 2)
+                        {
+                            throw new ConsoleException("Синтаксическая ошибка.");
+                        }
+                        else if (!command[1].Contains("*"))
+                        {
+                            Del(command[1]);
+                        }
+                        else
+                        {
+                            DelMany(command[1]);
+                        }
+                    }
+                    catch
+                    {
+                        message = "Синтаксическая ошибка. Воспользуйтесь коммандой help для вывода доступных комманд.\n";
+                        rtbConsole.Text += message;
+                    }
+                }
+
+                else if (command[0] == "")
+                {
+                    throw new ConsoleException("Введите команду");
+                }
+
+                else if(command[0] == "help")
+                {
+                    rtbConsole.Text += "cd .. - переход в родительский каталог\n";
+                    rtbConsole.Text += "cd *path* - изменить каталог\n";
+                    rtbConsole.Text += "rm *file/directory name* - удалить файл/директорию";
+
+
+                }
+                else
+                {
+                    throw new ConsoleException("Команда не распознана.");
+                }
+            }
+            catch
+            {
+                message = "Команда не распознана. Воспользуйтесь командой help для получения списка доступных команд.\n";
+                rtbConsole.Text += message;
+            }
+            finally
+            {
+                DirShow();
+                tbInput.Clear();
+            }
+        }
+
+        private void btnDirBack_Click(object sender, EventArgs e)
+        {
+            DirBack();
+        }
+    }
+}
